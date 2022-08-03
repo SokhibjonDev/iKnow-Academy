5140919e083b8795f80c393caee26e71a34f7f05
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-03 16:07:05 +0500

    feat : open Media style

---
+++ public/js/catalog.hover.js
@@ -0,0 +1,6 @@
+const room = document.querySelectorAll('.room');
+const li = document.querySelector('.lessonInfo');
+
+li.addEventListener('click', () => {
+    
+})
--- public/js/script.scroll.js
+++ public/js/script.scroll.js
@@ -3,8 +3,24 @@ window.addEventListener(
     function () {
         if (window.scrollY > 80) {
             document.querySelector("nav").style.background = "#fff";
+            document.querySelector("nav").style.boxShadow = "0 0 7px 1px #2c2c2c";
+
         } else {
             document.querySelector("nav").style.background = "#fff";
+            document.querySelector("nav").style.boxShadow = "none";
+            document.querySelector("nav").style.transition = ".2s";
+        }
+    }
+);
+window.addEventListener(
+    "scroll",
+    function () {
+        if (window.scrollY > 480) {
+            document.querySelector(".fst-half").style.opacity = "1";
+
+        } else {
+            document.querySelector(".fst-half").style.opacity = "0";
+            document.querySelector(".fst-half").style.transition = "1.3s";
         }
     }
 );
